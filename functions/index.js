d2b6af39008deac59d3bfddb1decbdb93f24c9ef
Gary McGhee <gary@buzzware.com.au>
2020-03-31 00:26:58 +0800

    mostly replaced from firebase-auth-example

--- functions/index.js
+++ functions/index.js
@@ -1,20 +1,67 @@
+const _ = require('lodash');
+var fs = require('fs');
+var path = require('path');
+
+const Cookie = require('tough-cookie').Cookie;
+
 const functions = require('firebase-functions');
-const fs = require('fs');
-const path = require('path');
-const GetSessionToken = require('./commands/GetSessionToken');
-exports.index = functions.https.onRequest((req, res) => {
-	let token = GetSessionToken.call(req);
-	if (token) {
-		var viewerHTML = fs.readFileSync(path.resolve(__dirname, 'index-template.html')).toString();
-		res.send(viewerHTML);
-	} else {
-		res.status(302);
-		res.set({
-			'Content-Type': 'text/html',
-			'Location': '/login',
-			'Cache-Control': 'private, no-cache, no-store, must-revalidate',
-			'Access-Control-Allow-Credentials': 'true'
-		});
-		res.send();
-	}
-});
+
+const express = require("express");
+const { wrap } = require('@awaitjs/express');
+
+function getSessionToken(req) {
+  let session;
+  if (req.headers.authorization && req.headers.authorization.startsWith('Bearer ')) {
+    session = req.headers.authorization.split('Bearer ')[1];
+    if (session)
+      return session;
+  }
+
+  session = req.cookies && req.cookies.__session;
+  if (session)
+    return session;
+
+  const raw_cookies = req.get('cookie');
+  if (!raw_cookies)
+    return null;
+
+  let cookies = {};
+  for (let rc of raw_cookies.split('; ')) {
+    let c = Cookie.parse(rc);
+    if (!c)
+      continue;
+    cookies[c.key] = c;
+  }
+
+  session = cookies && cookies['__session'];
+  return session;
+}
+
+let expressApp = express();
+
+expressApp.get('/', wrap(async function (req, res) {
+  try {
+    let token = getSessionToken(req);
+    if (token) {
+        var viewerHTML = fs.readFileSync(path.resolve(__dirname, 'index.html')).toString();
+      res.send(viewerHTML).end();
+    } else {
+      res.status(302);
+      res.set({
+      'Content-Type': 'text/html',
+      'Location': '/login',
+      'Cache-Control': 'private, no-cache, no-store, must-revalidate',
+      'Access-Control-Allow-Credentials': 'true'
+      });
+      res.send().end();
+    }
+  } catch (e) {
+    const errorHtml = `<!DOCTYPE html><html><head><title></title></head><body>
+					${e.message}
+					</body></html>`;
+    res.status(400).type('html');
+    res.end(errorHtml);
+  }
+}));
+
+exports.front = functions.https.onRequest(expressApp);
--- readme.md
+++ readme.md
@@ -3,21 +3,15 @@
 
 by Gary McGhee
 
-The idea of this is to set up a well engineered prototype with the following features :
+Please refer to the article https://medium.com/@gazmcghee/secure-authenticated-single-page-application-spa-hosting-with-firebase-8732b13af628
+for an indepth explanation.
 
-* a SPA (eg. Ember.js) hosted at the root index - here its just a simple page
-* The index is served by a function, and only if you have logged in. If you are not logged in, you can't get the index.html, and because the 
-javascript files are fingerprinted, the filename is unguessable, so it can be hosted public on a CDN while being relatively secure.
-* If you are not logged in, the index will redirect you to /login for the login page.
+Notes : 
+
+* a SPA (eg. Ember.js) would be hosted at the root index - here its just a simple page
 * The login page uses firebaseui
 * The login page takes the login then sets the cookie in the browser with the expiry date from the login token. The cookie is not made 
 secure if you are hosting at localhost, so it will work with http
-* If you successfully log in, you will be redirected to the index, and should get the html page
-* You could manipulate the html eg. inject configuration for the logged in user
-* You could redirect based on eg. user roles
-* If a cold start is required, it will happen when first loading your page, and requests after that should be fast
 
- Partly inspired by ["RailsConf 2014 Lightning Fast Deployment of Your Rails-backed JavaScript app" by Luke Melia](https://www.youtube.com/watch?v=QZVYP3cPcWQ)
- 
  
  
